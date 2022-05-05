a6863a0204b51dbbffd23ce4e8725f3e84158fee
TomasGl <globistomas@gmail.com>
2022-05-05 14:30:54 +0300

    Add argparse

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from os import system, remove
+from os import system
+import tempfile
+import argparse
+import pathlib
+
 
 MEMORY_START_ADDRESS = 0x402000
 MEMORY_SIZE = 0x1000
-TEMP_ASM_FILENAME = '.code.asm'
-TEMP_OBJ_FILENAME = '.code.o'
 TEMPLATE_FILENAME = 'template.asm'
 
 
-def compile(bin_filename):
-    compile_commands = (
-        f'nasm -f elf64 {TEMP_ASM_FILENAME} -o {TEMP_OBJ_FILENAME}',
-        f'ld -m elf_x86_64 -s -o {bin_filename} {TEMP_OBJ_FILENAME}'
-    )
-    for command in compile_commands:
-        system(command)
-    remove(TEMP_ASM_FILENAME)
-    remove(TEMP_OBJ_FILENAME)
+def compile(inputfile, outputfile):
+    with tempfile.NamedTemporaryFile(suffix='.o') as tmp_file:
+        compile_commands = (
+            f'nasm -f elf64 {inputfile} -o {tmp_file.name}',
+            f'ld -m elf_x86_64 -s -o {outputfile} {tmp_file.name}'
+        )
+        for command in compile_commands:
+            system(command)
 
 
 PTR_INCREMENT = '\tINC rsi\n'
@@ -40,9 +41,8 @@ class Translator:
     loop_index = -1
     loop_stack = list()
 
-    def load_bf_code(self, filename):
-        with open(filename) as bf_fd:
-            self.bf_code = bf_fd.read().strip()
+    def load_bf_code(self, bf_fd):
+        self.bf_code = bf_fd.read().strip()
 
     def _add_asm_opcode(self, opcode):
         self.asm_code += opcode
@@ -72,25 +72,47 @@ class Translator:
         with open(TEMPLATE_FILENAME) as temp_fd:
             self.template = temp_fd.read()
 
-    def save(self, filename):
+    def save(self, fd):
         result = self.template.format(
             MEMORY_SIZE=hex(MEMORY_SIZE),
             MEMORY_START_ADDRESS=hex(MEMORY_START_ADDRESS),
             CODE=self.asm_code
         )
-        with open(TEMP_ASM_FILENAME, 'w') as result_fd:
-            result_fd.write(result)
-        print(result)
-        compile(filename)
+        fd.write(result)
 
 
 if __name__ == '__main__':
-    from sys import argv
-    if len(argv) != 3:
-        print('Usage: python3 {} IN_BRAINFUCK OUT_BIN'.format(argv[0]))
-        exit(1)
+    import sys
+
+    parser = argparse.ArgumentParser(description='BrainFuck to amd64 assembler translator')
+    parser.add_argument('-i', '--in',
+                        dest='brainfuck_input',
+                        type=argparse.FileType('r'),
+                        help='Input brainfuck source filename',
+                        required=True
+                        )
+    parser.add_argument('-o', '--out',
+                        dest='asm_output',
+                        nargs='?',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        help='Output asm file (default stdout)'
+                        )
+    parser.add_argument('-c', '--compile',
+                        dest='bin_output',
+                        type=pathlib.Path,
+                        help='Compile asm (required nasm)'
+                        )
+    args = parser.parse_args()
+
     translator = Translator()
-    translator.load_bf_code(argv[1])
+    translator.load_bf_code(args.brainfuck_input)
     translator.translate()
     translator.load_template()
-    translator.save(argv[2])
+    if not (args.bin_output and args.asm_output == sys.stdout):
+        translator.save(args.asm_output)
+    if args.bin_output:
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.asm') as tmp_file:
+            translator.save(tmp_file)
+            tmp_file.flush()
+            compile(tmp_file.name, args.bin_output)
